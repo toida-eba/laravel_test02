00d799d18419305fa1c2920a0f868d75f415fb2e
t.toida <t.toida@ebacorp.jp>
2020-09-14 12:50:27 +0900

    create.blade.php修正

--- test02/resources/views/photos/create.blade.php
+++ test02/resources/views/photos/create.blade.php
@@ -1,3 +1,19 @@
+@extends('layouts.default_photo')
+@section('title', 'ファイルアップロード機能')
+@section('content')
+
+@if (session('status'))
+<div class="alert alert-success" role="alert" onclick="this.classList.add('hidden')">{{ session('status') }}</div>
+@endif
+
+{!! Form::open(['url' => 'photos/store', 'files' => true,'method' => 'post']) !!}
+{!! Form::label('fileName', 'アップロード') !!}
+{!! Form::file('fileName') !!}
+{!! Form::submit('アップロードする') !!}
+{!! Form::close() !!}
+
+<hr color="#000000" style="height:1px;">
+
 @foreach ($photos as $photo)
 <div class="panel panel-default">
   <div class="panel-heading">アップロードした日付：{{$photo->created_at}}</div>
@@ -7,3 +23,5 @@
   </ul>
 </div>
 @endforeach
+
+@endsection
