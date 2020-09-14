e372f60d11e1856c0ccabf9b46500d3e4ea770b5
t.toida <t.toida@ebacorp.jp>
2020-09-14 12:01:36 +0900

    storeメソッド追加

--- test02/app/Http/Controllers/UploadController.php
+++ test02/app/Http/Controllers/UploadController.php
@@ -7,8 +7,14 @@ use Illuminate\Http\Request;
 class UploadController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
 
     	return view('index');
     }
+
+    public function store(Request $request)
+    {
+        　dd($request->all());
+     }
 }
