42aa2c6d279550bce31d3fbbea2c0a973b464efb
t.toida <t.toida@ebacorp.jp>
2020-09-14 16:57:04 +0900

    auth設定

--- test02/routes/web.php
+++ test02/routes/web.php
@@ -17,3 +17,7 @@
 
 // Route::resource('/upload', 'UploadController');
 Route::resource('photos', 'PhotosController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
